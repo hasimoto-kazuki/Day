bdb2b352809ab72b33a6e5df793c9623fbd99c85
kazuki <kitatanabe0709@gmail.com>
2020-09-01 01:58:47 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,60 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark" style="background :linear-gradient(to right, #330000 0%,
+    #330000 10%, #660000 10%, #660000 20%, #330000 20%, #330000 30%, #660000 30%, #660000 40%, #330000 40%, #330000 50%,
+    #660000 50%, #660000 60%, #330000 60%, #330000 70%, #660000 70%, #660000 80%, #330000 80%, #330000 90%, #660000 90%, #660000 100%);">
+        <!--height :80px;-->
+        
+        {{-- トップページへのリンク --}}
+        <a class="navbar-brand" href="/"><i class="fas fa-home" style="margin-right: 5px;"></i>P_share</a>
+        
+                    
+
+        
+
+        
+                @if (Auth::check())
+                
+                 <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+                    <span class="navbar-toggler-icon"></span>
+                 </button>
+                    
+                    <div class="collapse navbar-collapse" id="nav-bar">
+                    <ul class="navbar-nav mr-auto"></ul>
+                    <ul class="navbar-nav">
+                    
+                    {{-- ユーザ一覧ページへのリンク --}}
+                    
+                    <!--<li class="nav-item">{!! link_to_route('users.index', 'ユーザー一覧', [], ['class' => 'nav-link']) !!}</li>-->
+                    
+                    {{-- ユーザー検索ページへのリンク --}}
+                    
+                    <!--<li class="nav-item">{!! link_to_route('search', 'ユーザー検索', [], ['class' => 'nav-link']) !!}</li>-->
+                    
+                    {{-- ランキングページへのリンク --}}
+                    
+                    <!--<li class="nav-item">{!! link_to_route('ranking', 'ランキング', [], ['class' => 'nav-link']) !!}</li> -->
+                    
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            {{-- ユーザ詳細ページへのリンク --}}
+                            <!--<li class="dropdown-item">{!! link_to_route('users.show', 'マイプロフィール', ['user' => Auth::id()]) !!}</li>-->
+                            
+                            {{-- ユーザ編集ページへのリンク --}}
+                            <!--<li class="dropdown-item">{!! link_to_route('users.edit', '編集', ['user' => Auth::id()]) !!}</li>-->
+                            
+                            <li class="dropdown-divider"></li>
+                            {{-- ログアウトへのリンク --}}
+                            <!--<li class="dropdown-item">{!! link_to_route('logout.get', 'ログアウト') !!}</li>-->
+                        </ul>
+                    </li>
+                
+                
+                </ul>
+            </div>
+                
+                @endif
+            
+        
+    </nav>
+</header>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>P_share</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+        <!--追加機能 削除する前の確認機能-->
+        <script language="JavaScript" type="text/JavaScript">
+             
+            function Delete_check(){
+                let checked = confirm("delete?");
+                if (checked == true) {
+                    return true;
+            } else {
+                    return false;
+            }
+          }
+          //追加機能文字数カウント
+          function ShowLength( str ) {
+	            str=str.replace(/\n/g, ""); 
+                document.getElementById("inputlength").innerHTML = ""+ str.length ;
+             }
+
+        </script> 
+        <!--追加機能ここまで-->
+        
+    </head>
+    
+    
+    <body>
+    <!--<body style="background:url({{ asset('images/image02.jpg') }}) no-repeat center center; background-size:cover; background-color:rgba(255,255,255,0.5);-->
+    <!--background-blend-mode:lighten; background-attachment: fixed;">-->
+    
+    <style>
+        body::before {
+  content: "";
+  background:url({{ asset('images/image02.jpg') }}) no-repeat center center;
+  background-color:rgba(255,255,255,0.5);
+  background-blend-mode:lighten;
+  background-size: cover;
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+  transform: translateZ(0);
+  }
+    </style>
+    
+    
+        
+    
+
+        {{-- ナビゲーションバー --}}
+        @include('commons.navbar')
+
+        <div class="container">
+            {{-- エラーメッセージ --}}
+            @include('commons.error_messages')
+
+            @yield('content')
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+        
+        
+    </body>
+</html>
